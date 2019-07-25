447d34b436f1803d60828c6dfd7850941d798d1c
giscafer <giscafer@outlook.com>
2019-07-25 18:25:36 +0800

    fix: corn timer

--- corn.js
+++ corn.js
@@ -46,7 +46,7 @@ module.exports = function(bot) {
   //   console.log(_bot);
   // 每天，8点整同步数据
   let job_datasync = new CronJob(
-    `* 8 * * *`,
+    `* * 8 * * *`,
     () => {
       Toutiao.generateDataJson();
       Kr36.generateDataJson();
@@ -58,7 +58,7 @@ module.exports = function(bot) {
 
   // 每周一推送头条信息
   let job_toutiao_notice = new CronJob(
-    `54 8 * * 1`,
+    `* 54 8 * * 1`,
     () => {
       let articles = Toutiao.getData();
       publish(articles, config.rss.groupIds);
@@ -69,7 +69,7 @@ module.exports = function(bot) {
   );
   // 每天，9点00分推送36kr信息
   let job_kr36_notice = new CronJob(
-    `40 8 * * *`,
+    `* 40 8 * * *`,
     () => {
       let articles = Kr36.getData();
       publish(articles, config.rss.kr36GroupIds);
@@ -80,7 +80,7 @@ module.exports = function(bot) {
   );
   // 即刻早报
   let job_jike_notice = new CronJob(
-    `50 8 * * *`,
+    `* 50 8 * * *`,
     () => {
       Jike.getData().then(content => {
         publish(content, config.rss.jikeGroupIds);
