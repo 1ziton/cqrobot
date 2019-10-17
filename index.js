b614f053ee5daca77ceb2a733098334f9afbfedb
giscafer <giscafer@outlook.com>
2019-10-17 18:14:39 +0800

    test: again

--- index.js
+++ index.js
@@ -91,11 +91,13 @@ bot.on('message', context => {
     console.log(
       'devops_admin.includes(user_id)=' + devops_admin.indexOf(user_id) !== -1
     );
+    console.log('user_id=' + user_id);
+    console.log((raw_message.includes('合并') && devops_admin.indexOf(user_id) !== -1))
+
     if (raw_message.includes('合并') && devops_admin.indexOf(user_id) !== -1) {
-      console.log('user_id=' + user_id);
       let idx = message.indexOf('合并');
       let text = raw_message.substring(idx + 2) || '';
-      // console.log('。。。。进来了。。。。。', text);
+      console.log('。。。。进来了。。。。。', text);
       text = text.trim();
       if (!text) return;
       let arr = text.split('#');
